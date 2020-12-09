86f7033dc0603fac1face050960c0bb40adb0081
Chin <zx255235@gmail.com>
2020-12-09 17:17:27 +0800

    Update 2020-12-09T09:17:19.100Z

---
+++ js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var newFn1 = function newFn1() {
+  console.log('11111');
+};
+
+newFn1();
+"use strict";
+
+var newFn2 = function newFn2() {
+  console.log('aaaa');
+};
+
+newFn2();
+//# sourceMappingURL=all.js.map
